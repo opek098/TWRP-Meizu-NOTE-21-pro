577c789db94b3c6fe4012ba9c4de1a50a602032a
opek098 <opekxfurina@gmail.com>
2026-04-22 21:50:22 +0700

    Initial commit: Device tree for M4125H

---
+++ Android.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE), MEIZU-M4125H)
+
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+endif
---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/twrp_M4125H.mk
+
+COMMON_LUNCH_CHOICES := \
+	twrp_M4125H-eng \
+	twrp_M4125H-userdebug \
+	twrp_M4125H-user
